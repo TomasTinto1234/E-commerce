2beeb089ff50f3d266d12efe415c72700b66e75c
TomasTinto1234 <tititinto@gmail.com>
2022-11-17 17:02:58 -0300

    ml

--- src/components/home/navBar.jsx
+++ src/components/home/navBar.jsx
@@ -47,11 +47,11 @@ return (
       <div className ="contenido">
         <header>
           <div className="contenido-header">
-         <a className="pr" href="#inicio" ><h1 className="sonh1">E-commerce</h1></a>
+         <a className="pr" href="#inicio" ><h1>E-commerce</h1></a>
           <div className="contenedor-foto">
          <a className="pr" href="#inicio" ><img src={carrito} height="55, 65"/></a>
       </div>
-         <nav id="elnav" className="elnav" onClick={()=>responsiveMenu()}>
+         <nav id="elnav" onClick={()=>responsiveMenu()}>
             <ul id="links" >
              <li className="pages"><a href="#products" >PRODUCTOS</a></li>
              <li className="pages"><a href="#categories" >CATEGORIAS</a></li>
@@ -75,37 +75,7 @@ return (
                 onMouseOver={({ target }) => (target.style.color = "#F9B621")}
                 onMouseOut={({ target }) => (target.style.color = "white")}
                 cursor="pointer"/>
-            {/* <i class="fa-solid fa-bars"></i> */}
-                <div className="redes">
-                <a className="pr" href="https://wa.me/+541161263966" target="_blank"><i className="fa-brands fa-whatsapp"></i></a>
-                <a className="pr" href="https://www.linkedin.com/in/tomas-tinto-320a85236/" target="_blank"><i className="fa-brands fa-linkedin"></i></a>
-                <a className="pr" href="https://github.com/TomasTinto1234" target="_blank"><i className="fa-brands fa-github"></i></a>
-                <a className="pr" href="https://www.instagram.com/tomas_tinto/" target="_blank"><i className="fa-brands fa-instagram"></i></a>
-                </div>
           </div>
-           {/* <nav className="elnav">
-            <ul id="links" className="losul">
-             <li className="pages"><a href="#inicio" className ="seleccionado" >INICIO</a></li>
-             <li className="pages"><a href="#products" >PRODUCTOS</a></li>
-             <li className="pages"><a href="/Carrito"><Link to={`/Carrito` }><BsCart4 color="white"
-                size="25px"
-                onMouseOver={({ target }) => (target.style.color = "#F9B621")}
-                onMouseOut={({ target }) => (target.style.color = "white")}
-                cursor="pointer"/></Link></a></li>
-             <li className="pages"><a href="/login"><Link to={"/Login"}><BiUserCircle color="white"
-                size="25px"
-                onMouseOver={({ target }) => (target.style.color = "#F9B621")}
-                onMouseOut={({ target }) => (target.style.color = "white")}
-                cursor="pointer"/></Link></a></li>
-             <li className="pages"><a href="#categories">CATEGORIAS</a></li>
-             <li className="pages"><a href="#users" >USUARIOS</a></li>
-             <li className="pages"><a href="#contacto">CONTACTO </a></li>
-            </ul>
-          <div id="icono-nav" >
-            <i className="fa-solid fa-bars"></i>
-          </div>
-           </nav> */}
-
           </div>
         </header>
       </div>
